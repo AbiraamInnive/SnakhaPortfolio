9564024fb6d992382e8ef9ff20ab2fd67a934c94
Abiraam <109072164+AbiraamInnive@users.noreply.github.com>
2025-07-07 08:53:13 +0530

    Update index.js

--- src/components/aboutMe/index.js
+++ src/components/aboutMe/index.js
@@ -4,12 +4,12 @@ import { FaLinkedin, FaInstagram, FaWhatsapp, FaEnvelope } from 'react-icons/fa'
 export default function Footer() {
     return (
         <footer id="aboutMe" className="aboutMeContainer py-10">
-            <div className="mx-auto px-4 sm:px-6 md:px-24 flex flex-col md:flex-row justify-between items-start gap-y-10 md:gap-x-20">
+            <div className="mx-auto px-4 sm:px-6 md:px-24 flex flex-col md:flex-row justify-between items-start md:gap-x-20">
 
                 {/* Profile image + About */}
-                <div className="flex flex-col sm:flex-row gap-6 w-full md:w-[70%]">
+                <div className="flex flex-col sm:flex-row  gap-x-10 w-full md:w-[70%]">
                     {/* Profile image */}
-                    <div className="flex md:flex-col sm:flex-row md:items-center space-y-3">
+                    <div className="flex md:flex-col sm:flex-row space-x-5 sm:space-x-5 md:items-center space-y-3">
                         <div className="relative  w-32 h-32 sm:w-26 sm:h-26 rounded-lg overflow-hidden shadow-md">
                             <Image
                                 src="/elements/profile.jpg"
@@ -19,13 +19,13 @@ export default function Footer() {
                             />
                         </div>
                         <div className="text-center sm:w-1/2 md:w-full">
-                            <p className="font-semibold text-lg">Snakha Ranjan</p>
+                            <p className="font-semibold text-lg md:text-nowrap">Snakha Ranjan</p>
                             <p className="text-sm">B.Arch</p>
                         </div>
                     </div>
 
                     {/* About Me Text */}
-                    <div className="mt-3 md:w-1/2 sm:mt-0">
+                    <div className="mt-3 md:w-3/4 sm:mt-0">
                         <h3 className="text-xl font-semibold mb-2">About Me</h3>
                         <p className="text-sm leading-relaxed">
                             I'm a passionate architect and visual storyteller, blending creativity and functionality to design immersive spaces. From concept to reality, I turn visions into tangible designs.
@@ -45,7 +45,7 @@ export default function Footer() {
                             href="mailto:snakharanjan@gmail.com"
                             className="text-sm text-nowrap"
                         >
-                            snakharanjan@gmail.com
+                            Email
                         </a>
                     </div>
 
